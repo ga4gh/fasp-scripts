f85030e47b393fcf2547df141c7fa7e5937a75c9
Ian Fore <ian.fore@nih.gov>
2020-09-17 11:23:45 -0400

    Update SearchScriptGeccoPP.py
    Phenopacket join debugging

--- SearchScriptGeccoPP.py
+++ SearchScriptGeccoPP.py
@@ -16,17 +16,17 @@ def main(argv):
 
 	dbgap_query3 = """select sm.biosample_accession FROM dbgap_demo.scr_gecco_susceptibility.sample_multi sm"""
 
-	query_job = searchClient.runQuery(dbgap_query2)  # Send the query
-	print(len(query_job))
+#	query_job = searchClient.runQuery(dbgap_query2)  # Send the query
+#	print(len(query_job))
 # 	for r in query_job:
 # 		print (r)
 		
-	pp_dbgap_join = """select sm.biosample_accession, sp.sex dbgap_sex, json_extract_scalar(phenopacket, '$.subject.sex') pp_sex FROM dbgap_demo.scr_gecco_susceptibility.subject_phenotypes_multi sp join dbgap_demo.scr_gecco_susceptibility.sample_multi sm on sm.dbgap_subject_id = sp.dbgap_subject_id join sample_phenopackets.ga4gh_tables.gecco_phenopackets pp on pp.id = sm.biosample_accession"""
+	pp_dbgap_join = "select sm.biosample_accession, sp.sex dbgap_sex, json_extract_scalar(pp.phenopacket, '$.subject.sex') FROM dbgap_demo.scr_gecco_susceptibility.subject_phenotypes_multi sp join dbgap_demo.scr_gecco_susceptibility.sample_multi sm on sm.dbgap_subject_id = sp.dbgap_subject_id join sample_phenopackets.ga4gh_tables.gecco_phenopackets pp on pp.id = sm.biosample_accession"
 
-	query_job = searchClient.runQuery(dbgap_query3)  # Send the query
+	query_job = searchClient.runQuery(pp_dbgap_join)  # Send the query
 	print(len(query_job))
-# 	for r in query_job:
-# 		print (r)
+	for r in query_job:
+		print (r)
 		
     
 if __name__ == "__main__":
