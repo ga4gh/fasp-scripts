188c304d24a5fab70f719284552d5e9cf9e96d3b
Ian Fore <ian.fore@nih.gov>
2020-11-06 14:51:35 -0500

    Added example covid queries

--- fasp/scripts/ga4gh_search_covid_example.py
+++ fasp/scripts/ga4gh_search_covid_example.py
@@ -13,16 +13,19 @@ def main(argv):
 	# Step 1 - Discovery
 	# query for relevant DRS objects
 	
-	searchClient = DiscoverySearchClient('https://search-presto-public-covid19.prod.dnastack.com')
+	searchClient = DiscoverySearchClient('https://search-presto-public-covid19.prod.dnastack.com/')
 	
-	# List table 
-	searchClient.listTables()
+	# List tables
+	#searchClient.listTables()
 	
-	#query = ""
-		
-	#res = searchClient.runQuery(query)
+	# List table schema
+	#searchClient.listTableInfo('coronavirus_dnastack_curated.covid_cloud_production.sequences')
+	
+	
+	query = 'select accession, biosample, genus, species from coronavirus_dnastack_curated.covid_cloud_production.sequences limit 10'
+	res = searchClient.runQuery(query)
+	print(res)
 	
 
-    
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	main(sys.argv[1:])
