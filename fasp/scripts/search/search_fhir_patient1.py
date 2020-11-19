1d78b6bc6b86d62037e4503bcc8eb49cee0c07f8
Ian Fore <ian.fore@nih.gov>
2020-11-19 11:34:19 -0500

    Add codeathon examples

---
+++ fasp/scripts/search/search_fhir_patient1.py
@@ -0,0 +1,26 @@
+''' Basic query on NCPI FHIR server Patient resource via DNAStack Search test implementation'''
+#  IMPORTS
+import sys 
+import json
+
+
+from fasp.search  import DiscoverySearchClient
+
+
+
+
+def main(argv):
+
+	searchClient = DiscoverySearchClient('https://ga4gh-search-adapter-presto-public.prod.dnastack.com')
+
+	
+	
+	query = 'select id, patient from kidsfirst.ga4gh_tables.patient limit 3'
+	res = searchClient.runQuery(query)
+
+	print(json.dumps(res, indent=2))
+
+	
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
