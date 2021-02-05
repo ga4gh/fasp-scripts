27c28162d2bdbaf4c4d3e1017d0351f205b49406
Ian Fore <ian.fore@nih.gov>
2021-02-04 23:17:18 -0500

    Scripts to notebooks and pruning

--- fasp/workflow/__init__.py
+++ fasp/workflow/__init__.py
@@ -1,6 +1,8 @@
 from fasp.workflow.wesclient import WESClient
 from fasp.workflow.dnastack_wesclient import DNAStackWESClient
 from fasp.workflow.sb_wes_client import sbWESClient
+from fasp.workflow.sb_wes_client import sbcgcWESClient
+from fasp.workflow.sb_wes_client import cavaticaWESClient
 
 from fasp.workflow.gcpls_samtools import GCPLSsamtools
 
