7dbe704b467ab6d2ca7e3fbb5be79e1960108927
Max Barkley <max@dnastack.com>
2021-06-24 11:20:36 -0400

    WIP get FASPScript19 running

--- setup.py
+++ setup.py
@@ -2,6 +2,22 @@
 
 from setuptools import setup
 
+install_requires = [
+    'applescript',
+    'google-api-python-client',
+    'google-cloud-bigquery',
+    'google-cloud-storage',
+    'ipykernel',
+    'notebook',
+    'oauth2client',
+    'pandas',
+    'Pillow',
+    'pyega3',
+    'requests',
+    'sevenbridges-python',
+    'humanize'
+]
+
 setup(name='fasp',
       version='1.1',
       packages=['fasp',
@@ -10,4 +26,5 @@ setup(name='fasp',
                 'fasp.loc',
                 'fasp.workflow'],
       include_package_data=True,
+      install_requires=install_requires
       )
