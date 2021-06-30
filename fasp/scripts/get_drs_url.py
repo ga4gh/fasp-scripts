7351348e1087bc7fffc1aa515102928d8760b0aa
Ian Fore <ian.fore@nih.gov>
2021-06-30 16:04:19 -0400

    Update get_drs_url.py
    Comments on input for get_drs_url

--- fasp/scripts/get_drs_url.py
+++ fasp/scripts/get_drs_url.py
@@ -8,7 +8,10 @@ from fasp.loc import anvilDRSClient
 
 def main(argv):
 
+	# The DRS id for the file required 
 	drs_id = argv[0]
+	# Your Google Cloud project id to which any egress charges would be billed
+	# If you run the compute in the same GCP region there should be no egress charges
 	gcp_project = argv[1]
 	# edit the following line for where you put your credentials file from anvil
 	credentials_file = '~/.keys/anvil_credentials.json'
