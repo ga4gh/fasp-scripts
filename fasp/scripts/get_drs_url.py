c29597a8762b29adcd9abf1ea995332c9e5d6d08
Ian Fore <ian.fore@nih.gov>
2021-06-30 16:18:46 -0400

    added s3 example to get_drs_url

--- fasp/scripts/get_drs_url.py
+++ fasp/scripts/get_drs_url.py
@@ -12,10 +12,17 @@ def main(argv):
 	drs_id = argv[0]
 	# Your Google Cloud project id to which any egress charges would be billed
 	# If you run the compute in the same GCP region there should be no egress charges
+	# Not required if working on AWS
 	gcp_project = argv[1]
 	# edit the following line for where you put your credentials file from anvil
 	credentials_file = '~/.keys/anvil_credentials.json'
 
+	# Amazon Web Services
+	drsClient = anvilDRSClient(credentials_file, 's3')
+	url = drsClient.getAccessURL(drs_id)
+	print(url)
+
+	# Google Cloud
 	drsClient = anvilDRSClient(credentials_file, gcp_project, 'gs')
 	url = drsClient.getAccessURL(drs_id)
 	print(url)
