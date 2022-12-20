e6880b6a29b25f5e51358d3bd65eb723ddb33d7a
Juti Noppornpitak <juti@dnastack.com>
2022-12-20 10:47:29 +0700

    Replaced all references to the soon-to-be-decommissioned or already decommissioned Data Connect services with data.publisher.dnastack.com

--- scripts/search/getTableData.py
+++ scripts/search/getTableData.py
@@ -1,13 +1,13 @@
 import requests
 import pprint
 
-
-query = "{\"query\":\"with brca_genes as (select gene_symbol, count(*) as brca_count from search_cloud.brca_exchange.v32 group by gene_symbol) select bg.*, cv.* from brca_genes bg inner join search_cloud.clinvar.allele_gene cv on bg.gene_symbol=cv.symbol limit 1000\"}"
+# FIXME https://www.pivotaltracker.com/story/show/184043487
+query = "{\"query\":\"with brca_genes as (select gene_symbol, count(*) as brca_count from collections.public_datasets.v32 group by gene_symbol) select bg.*, cv.* from brca_genes bg inner join collections.public_datasets.allele_gene cv on bg.gene_symbol=cv.symbol limit 1000\"}"
 headers = {
   'content-type': 'application/json'
 }
 
-next_url = "https://ga4gh-search-adapter-presto-public.prod.dnastack.com/table/search_cloud.clinvar.allele_gene/data"
+next_url = "https://publisher-data.publisher.dnastack.com/data-connect/table/search_cloud.clinvar.allele_gene/data"
 
 pageCount = 0
 while next_url != None :
@@ -15,7 +15,7 @@ while next_url != None :
 	print ("____Page{}_______________".format(pageCount))
 	response = requests.request("GET", next_url)
 	result = (response.json())
-	pprint.pprint(result) 
+	pprint.pprint(result)
 	next_url = result['pagination']['next_page_url']
 
 #	print ("has {} data rows".format(len(result['data'])))
@@ -29,4 +29,4 @@ while next_url != None :
 # 		else:
 # 			print ("model is different than previous page")
 # 		last_model = dm
-			
+
